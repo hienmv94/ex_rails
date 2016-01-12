0d32dd9495fee366b3ca737d87410b53c0ca3bd3
Hienmv94 <hienmv94@gmail.com>
2016-01-12 10:09:25 +0700

    Make a basic User model (including secure password)

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -41,6 +41,14 @@ class UserTest < ActiveSupport::TestCase
     end
   end
 
+  test "email addresses should be saved as lower-case" do
+    mixed_case_email = "Foo@ExAMPle.CoM"
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal mixed_case_email.downcase, @user.reload.email
+  end
+
+
   test "email address should be unique" do
     duplicate_user = @user.dup
     duplicate_user.email = @user.email.upcase
