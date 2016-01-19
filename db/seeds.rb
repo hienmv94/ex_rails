5961f6b9b9b1f57a3210dec3a5e5f052a346dcdf
Hienmv94 <hienmv94@gmail.com>
2016-01-19 10:34:24 +0700

    done follow

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,3 +23,19 @@ users = User.order(:created_at).take(6)
   content = Faker::Lorem.sentence(5)
   users.each { |user| user.microposts.create!(content: content) }
 end
+
+
+# Following relationships
+users = User.all
+user  = users.first
+following = users[2..50]
+followers = users[3..40]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
+User.create!(name:  "Demo User",
+             email: "hienmv.nd@gmail.com",
+             password:              "mrhien1994",
+             password_confirmation: "mrhien1994",
+             admin: false,
+             activated: true,
+             activated_at: Time.zone.now)
